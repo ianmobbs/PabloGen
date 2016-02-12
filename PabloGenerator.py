84746de3f931ca17512a91dedd3bfd625294c7d3
Ian Mobbs <ianmobbs@gmail.com>
2016-02-11 22:23:25 -0600

    Changed font, added coordinates

--- PabloGenerator.py
+++ PabloGenerator.py
@@ -6,7 +6,7 @@ window = Tk()
 canvas = Canvas(width=1024, height=1024, background='#F78C58')
 coords = open('coords.txt', 'r')
 for line in coords:
-    canvas.create_text(int((line.rstrip().split(' '))[0]), int((line.rstrip().split(' '))[1]), anchor='nw', font='Helvetica 20 bold', text=phrase)
+    canvas.create_text(int((line.rstrip().split(' '))[0]), int((line.rstrip().split(' '))[1]), anchor='nw', font='Helvetica 29 bold', text=phrase)
 
 
 canvas.pack()
