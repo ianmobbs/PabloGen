709ac26fae8462cd44445b3831d681b49cb4709a
Ian Mobbs <ianmobbs@gmail.com>
2016-02-11 23:59:53 -0600

    Comments + slight restructure

--- PabloGenerator.py
+++ PabloGenerator.py
@@ -1,12 +1,30 @@
+#Import tkinter library for use to draw image
 from tkinter import *
 
+#Ask user for phrase, convert it to all upper case
 phrase = input("Enter phrase: ").upper()
 
-window = Tk()
-canvas = Canvas(width=1024, height=1024, background='#F78C58')
+#Establish all of our constants
+dimension = 1024
+bgcolor = '#F78C58'
 coords = open('coords.txt', 'r')
+
+#Create a Tkinter canvas
+window = Tk()
+canvas = Canvas(width=dimension, height=dimension, background=bgcolor)
+
+#Print lines of text to Canvas
 for line in coords:
-    canvas.create_text(int((line.rstrip().split(' '))[0]), int((line.rstrip().split(' '))[1]), anchor='nw', font='Helvetica 29 bold', text=phrase)
+	coord = line.rstrip().split(' ')
+	canvas.create_text(int(coord[0]), int(coord[1]), anchor='nw', font='Helvetica 29 bold', text=phrase)
 
-
+#Pack it all up!
 canvas.pack()
+
+###TODO###
+#Figure out how to export canvas as JPG
+#	Use PIL or QT or something to export?
+#	Draw in PIL or QT or something then export?
+#Create a Twitter bot, replies to all mentions with image using text
+#	Run off Heroku instance?
+#	Buy raspberry pi?
